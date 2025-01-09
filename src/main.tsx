645c690625f1a42c54ccf1d1db889c75d64375cf
IsaacSemb <isaacsemb1996@gmail.com>
2025-01-09 18:20:59 +0000

    added dark mode to the site

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,16 @@ import { createRoot } from "react-dom/client";
 
 // 3rd party imports
 // chakra
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 // in-house imports
 import App from "./App.tsx";
+import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>
