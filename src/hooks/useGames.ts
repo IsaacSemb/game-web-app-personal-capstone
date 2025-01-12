273010d31f867954faec520bb03243ce6a198f02
IsaacSemb <isaacsemb1996@gmail.com>
2025-01-12 04:50:57 +0000

    created skeletons for loading state of the game gards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,20 +24,27 @@ interface fetchingGamesResponse {
 function useGames() {
   const [games, setGames] = useState<GameObject[]>([]);
   const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
     apiClient
       .get<fetchingGamesResponse>("/games", { signal: controller.signal })
-      .then((response) => setGames(response.data.results))
+      .then((response) => {
+        setGames(response.data.results);
+        setIsLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setIsLoading(false);
       });
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 }
 
 export default useGames;
