afe4c6cf017807a8f3006b9a34cb7f1f4d19b130
IsaacSemb <isaacsemb1996@gmail.com>
2025-01-14 22:21:52 +0000

    Built platform selector and added custom hook for fetching the platforms

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 
 import { GenreObject } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<GenreObject | null>(null);
@@ -38,6 +39,7 @@ function App() {
       </Show>
 
       <GridItem area="main">
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
